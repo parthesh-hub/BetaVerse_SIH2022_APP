a97cf8a43e33bce705575313f61d0f962fc4a416
sanket <sanketjangale2014@gmail.com>
2022-03-08 14:05:34 +0530

    Added Transition Functionality

--- app/src/main/java/com/example/kaaryakhoj/MainActivity.java
+++ app/src/main/java/com/example/kaaryakhoj/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.kaaryakhoj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_verify_phone);
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,VerifyPhone.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kaaryakhoj/VerifyPhone.java
+++ app/src/main/java/com/example/kaaryakhoj/VerifyPhone.java
@@ -9,6 +9,6 @@ public class VerifyPhone extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_phone);
+        setContentView(R.layout.activity_main);
     }
 }
